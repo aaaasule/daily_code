adeef701e23ea4c9d8a1b6133ea35503f6720159
aaaasule123 <aaaasule123@163.com>
2019-08-02 17:40:44 +0800

    巩固一下冒泡排序

--- 08/0802/__init__.py
+++ 08/0802/__init__.py
@@ -4,3 +4,15 @@
 # @File    : __init__.py.py
 
 print('hello 0802')
+
+def bubble_sort(nums):
+    for i in range(len(nums)):
+        for j in range(len(nums)-1):
+            if nums[i] < nums[j]:
+                nums[i],nums[j] = nums[j],nums[i]
+
+
+
+n = [1,3,5,7,9,2,4]
+bubble_sort(n)
+print(n)
