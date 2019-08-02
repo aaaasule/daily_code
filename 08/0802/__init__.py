c026a14cd063f3cb4917ffce6e3a6a54924afb78
aaaasule123 <aaaasule123@163.com>
2019-08-02 18:11:01 +0800

    巩固一下冒泡、选择排序

--- 08/0802/__init__.py
+++ 08/0802/__init__.py
@@ -5,14 +5,25 @@
 
 print('hello 0802')
 
+# 冒泡排序
 def bubble_sort(nums):
     for i in range(len(nums)):
         for j in range(len(nums)-1):
             if nums[i] < nums[j]:
                 nums[i],nums[j] = nums[j],nums[i]
 
+# 选择排序
+def select_sort(nums):
+    for i in range(len(nums)-1):
+        min_index = i
+        for j in range(i+1,len(nums)):
+            if nums[min_index] > nums[j]:
+                min_index = j
+        nums[i],nums[min_index] = nums[min_index],nums[i]
 
 
-n = [1,3,5,7,9,2,4]
-bubble_sort(n)
+n = [8,3,5,7,9,2,4]
+# bubble_sort(n)
+select_sort(n)
 print(n)
+
