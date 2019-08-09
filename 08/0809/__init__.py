951df5df98fd8919e2540984457831261d51e1fd
aaaasule123 <aaaasule123@163.com>
2019-08-09 17:06:13 +0800

    没聊

--- 08/0809/__init__.py
+++ 08/0809/__init__.py
@@ -4,3 +4,13 @@
 # @File    : __init__.py.py
 
 print('一个要开一天会的日子')
+
+print('再次复习Python基础')
+
+print('Alex Li(金角大王)')
+
+print('CrazyEye堡垒机、Madking资产管理、TriAquae主机管理开源软件作者')
+
+print('松下、公安部、飞信、中金公司、华尔街英语、Nokia中国、Advent软件、汽车之家')
+
+print('Coding、旅游、电影、音乐、妹子')
