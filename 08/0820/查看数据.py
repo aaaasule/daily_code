736fe975b70233f82cff7f31393597358fd540fa
aaaasule123 <aaaasule123@163.com>
2019-08-21 15:43:48 +0800

    改一下readme

---
+++ 08/0820/查看数据.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/20 0020 10:58
+# @Author  : Zhang
+# @File    : 查看数据.py
+
+class Animal(object):
+
+    def __init__(self,name,age,*args,**kwargs):
+        self.name = name
+        self.age = age
+
+    def eat(self):
+        pass
+
+    def run(self):
+        pass
+
+    def create(self):
+        pass
+
+    def death(self):
+        pass
+
